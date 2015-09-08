08812bb09810c89994316b6218919c38a4de4f1a
LynxHH <jp.reimer@outlook.de>
2015-09-08 12:55:27 +0200

    login mit DB.User.login und auto-login

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,20 @@
 /**
  * Created by 3buchhar on 08.09.2015.
  */
+
+$(document).ready(function () {
+    if (DB.User.me) {
+        alter("Sie sind schon eingeloggt!");
+    } else {
+        $("#login-button").on("click", function () {
+            var username = $("#login-username-box").val();
+            var password = $("#login-password-box").val();
+            DB.User.login(username, password).then(function () {
+                $("#login-view").hide();
+                $("#dashboard-view").show();
+            }, function () {
+                alert("Login fehlgeschlagen!");
+            });
+        });
+    }
+});
