a9f0e307d3dba350c3dc7396fe861992cd865f7f
caleron <paddy.buchhardt@arcor.de>
2015-09-09 12:44:13 +0200

    registration view mit handlebars

--- scripts/module-registration.js
+++ scripts/module-registration.js
@@ -4,6 +4,26 @@ var moduleRegistrationController = {};
  * Wird bei dem Anzeigen der Modulanmeldungs-View ausgeführt
  */
 moduleRegistrationController.init = function () {
+    var mdViewSource = $("#md-view-template").html(),
+        mdViewTemplate = Handlebars.compile(mdViewSource),
+        detailListItemSource = $("#detail-list-item-template").html(),
+        detailListItemTemplate = Handlebars.compile(detailListItemSource),
+        registrationView = $("#registration-manager-view");
+
+    registrationView.html(mdViewTemplate({}));
+
+    var listItemContext = {
+      title: "SE1",
+        credits: 3,
+        responsible: "Axel",
+        precondition: "nix",
+        courses: "Vorlesung 1 <br> Übung 1",
+        description: "Axel bringt euch alles bei"
+    };
+    registrationView.find(".detail-view").html(detailListItemTemplate(listItemContext));
+    registrationView.find(".detail-view").append(detailListItemTemplate(listItemContext));
+    registrationView.find(".detail-view").append(detailListItemTemplate(listItemContext));
+
     $(".detail-list-item-header").on("click", function () {
         $(this).next().slideToggle();
     });
