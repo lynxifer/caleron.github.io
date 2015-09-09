f6e6f1dad459f011925d9bd7f06f1fe7a5954f44
caleron <paddy.buchhardt@arcor.de>
2015-09-09 12:28:35 +0200

    master detail view update

--- scripts/module-registration.js
+++ scripts/module-registration.js
@@ -4,7 +4,7 @@ var moduleRegistrationController = {};
  * Wird bei dem Anzeigen der Modulanmeldungs-View ausgeführt
  */
 moduleRegistrationController.init = function () {
-    $(".registration-module-list-item-header").on("click", function () {
+    $(".detail-list-item-header").on("click", function () {
         $(this).next().slideToggle();
     });
 };
