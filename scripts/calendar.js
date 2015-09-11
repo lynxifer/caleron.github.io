f9801f931765251a0b963b8545f0c65d792c30e9
LynxHH <jp.reimer@outlook.de>
2015-09-11 14:37:58 +0200

    calendar.css kalender design calendar.js in index.html

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -11,3 +11,22 @@ var calendarController = {};
 calendarController.init = function () {
 
 };
+
+/*TODO: +"1" noch ersetzen durch "Tag" aus Datenbank
+* Fehler*/
+
+/*
+calendarController.loadAppointments = function () {
+    var calendarAppointmentViewItemSource = $(calendar-appointment-view-template).html(),
+        calendarAppointmentViewTemplate = Handlebars.compile(calendarAppointmentViewItemSourceViewSource),
+        calendarAppointmentView = $("#calendar-day-column"+"1"),
+        request;
+
+    var listItemContext = {
+        title: 'Modulkürzel',
+        /!*content: news.content,*!/
+        time: 'Uhrzeit'
+    };
+
+    calendarAppointmentView.append(calendarAppointmentViewTemplate(listItemContext));
+}*/
