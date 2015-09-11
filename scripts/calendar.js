93a25ea6f30fd03a9b8cf2e21f43ca77b69b4b9f
LynxHH <jp.reimer@outlook.de>
2015-09-11 15:34:58 +0200

    calender-item template

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -9,24 +9,23 @@ var calendarController = {};
  */
 
 calendarController.init = function () {
-
+    calendarController.loadAppointments();
 };
 
 /*TODO: +"1" noch ersetzen durch "Tag" aus Datenbank
 * Fehler*/
 
-/*
 calendarController.loadAppointments = function () {
-    var calendarAppointmentViewItemSource = $(calendar-appointment-view-template).html(),
-        calendarAppointmentViewTemplate = Handlebars.compile(calendarAppointmentViewItemSourceViewSource),
-        calendarAppointmentView = $("#calendar-day-column"+"1"),
+    var calendarAppointmentViewItemSource = $("#calendar-appointment-view-template").html(),
+        calendarAppointmentViewTemplate = Handlebars.compile(calendarAppointmentViewItemSource),
+        calendarAppointmentView = $("#calendar-column-day1"),
         request;
 
     var listItemContext = {
-        title: 'Modulkürzel',
-        /!*content: news.content,*!/
-        time: 'Uhrzeit'
+        title: "Modul",
+        /*content: news.content,*/
+        time: "Zeit"
     };
 
     calendarAppointmentView.append(calendarAppointmentViewTemplate(listItemContext));
-}*/
+}
