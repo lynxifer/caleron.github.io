314eaa77698416b9456fa29cc10e26c786c719f4
caleron <paddy.buchhardt@arcor.de>
2015-09-13 18:27:13 +0200

    ansicht von anmeldungen funktioniert provisorisch

--- scripts/your-events.js
+++ scripts/your-events.js
@@ -39,7 +39,8 @@ eventController.loadDetailView = function (category) {
     console.log(category);
 
     var detailView = $("#events-view").find(".detail-view"),
-        request = DB.Registration.find({depth: 1}).equal("student", DB.User.me);
+        request = DB.Registration.find({depth: 1}).equal("student", DB.User.me),
+        modules = {};
 
     if (category) {
         if (category === "current") {
@@ -51,13 +52,59 @@ eventController.loadDetailView = function (category) {
         }
     }
 
-    request.resultList(function (result) {
+    /*request.resultList(function (result) {
         detailView.empty();
 
         result.forEach(function (registration) {
 
+            var module = registration.module;
+
+            if (modules[module.shortname] === undefined) {
+                modules[module.shortname] = registration.module;
+            }
+            if (modules[module.shortname]["courses"] === undefined) {
+                modules[module.shortname]["courses"] = [];
+            }
+            modules[module.shortname]["courses"].push(registration.course);
+
+        });
+    }).then(function () {
+     var moduleCount = modules.length;
+     for (var i = 0; i < moduleCount; i++) {
+     detailView.append("<p>" + modules[i].name + "</p>");
+     }
+     });*/
+    var promises = [];
+    request.resultList(function (result) {
+        detailView.empty();
+
+        result.forEach(function (registration) {
+            promises.push(registration.load({depth: 1}).then(function () {
+
+                var module = registration.module;
+
+                if (modules[module.shortname] === undefined) {
+                    modules[module.shortname] = registration.module;
+                }
+                if (modules[module.shortname]["courses"] === undefined) {
+                    modules[module.shortname]["courses"] = [];
+                }
+                modules[module.shortname]["courses"].push(registration.course);
+            }));
+
+        });
+        //BUG: DB.User.me.load() geht nicht, bzw generell user.load()
+        Promise.all(promises).then(function () {
+            console.log(modules);
+            window.mod = modules;
+
+            for (var a in modules) {
+                console.log(modules[a]);
+                detailView.append("<p>" + modules[a].name + "</p>");
+            }
         });
     });
+
 };
 
 eventController.onCategoryClick = function () {
